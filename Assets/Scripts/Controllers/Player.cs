e3a2ce33010dfe730298cb8f4e52958aecbddda8
GHDeveloper06 <costath@sheridancollege.ca>
2026-09-20 11:59:54 -0400

    Added function to player script that spawns a bomb at an offset from the player when the B key is pressed

---
+++ Assets/Scripts/Controllers/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    public Transform enemyTransform;
+    public GameObject bombPrefab;
+    public List<Transform> asteroidTransforms;
+
+    // Update is called once per frame
+
+    //create public Vector2 bomboffset
+    public Vector2 bombOffset = new Vector2(0, -1);
+    void Update()
+    {
+        if (Keyboard.current.bKey.wasPressedThisFrame) 
+        {
+            spawnBombAtOffset(transform.position);
+        }
+    }
+    //create method to spawn bomb
+    void spawnBombAtOffset(Vector2 playerPosition)
+    {
+        //Instantiate(bombPrefab, playerPosition + new Vector2 (0, -1), Quaternion.identity);
+        Instantiate(bombPrefab, playerPosition + bombOffset, Quaternion.identity);
+    }
+}
